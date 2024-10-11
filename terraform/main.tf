a070cd31707687f04c11a896e821c75c9d09bacc
Itsfoss0 <akhonyaebenezer@gmail.com>
2024-10-12 02:31:49 +0300

    [chore]: initial commit
    Summary:
    Setup hooks and CI pipeline

---
+++ terraform/main.tf
@@ -0,0 +1,76 @@
+terraform {
+  required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.0.0"
+    }
+  }
+}
+
+provider "kubernetes" {
+  config_path    = "~/.kube/config"
+  config_context = "minikube"
+}
+
+resource "kubernetes_deployment" "nginx" {
+  metadata {
+    name = "nginx-deployment"
+    labels = {
+      app = "nginx"
+    }
+  }
+
+  spec {
+    replicas = 1
+
+    selector {
+      match_labels = {
+        app = "nginx"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = "nginx"
+        }
+      }
+
+      spec {
+        container {
+          image = "nginx:alpine"
+          name  = "nginx"
+
+          port {
+            container_port = 80
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_service" "nginx" {
+  metadata {
+    name = "nginx-service"
+  }
+
+  spec {
+    selector = {
+      app = kubernetes_deployment.nginx.spec.0.template.0.metadata.0.labels.app
+    }
+
+    type = "NodePort"
+
+    port {
+      port        = 80
+      target_port = 80
+      node_port   = 30080
+    }
+  }
+}
+
+output "nginx_url" {
+  value       = "http://$(minikube ip):30080"
+  description = "URL to access the Nginx server"
+}
